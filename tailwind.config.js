f3ab33ec15381e18dec2712d582ab7b4f4a2005a
PhjnX <buihoangphi2003@gmail.com>
2024-10-22 09:06:28 +0700

    upload source

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'jost': ['Jost', 'sans-serif'],
+        'nunito': ['Nunito', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    
+  ],
+}
